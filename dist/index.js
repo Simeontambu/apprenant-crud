51ca22c2013b3037b342f7d72938826349509e76
simeontambu <simeontambu@gmail.com>
2024-03-26 21:26:08 +0100

    Feat: config server

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3001;
+const cors = require('cors');
+const dotenv = require('dotenv');
+dotenv.config();
+app.use(cors());
+app.use(express.json());
 app.listen(PORT, console.log("Server don start for port: " + PORT));
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,9 @@
 const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 3001;
+const cors = require('cors')
+const dotenv =require('dotenv')
+dotenv.config();
+app.use(cors())
+app.use(express.json())
 app.listen(PORT, console.log("Server don start for port: " + PORT))
