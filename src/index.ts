798e5fab37dc69d614b3979190f2d3021da1dd88
simeontambu <simeontambu@gmail.com>
2024-03-29 21:30:47 +0100

    Feat : auth

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,10 @@ import cohorteRouter from './routes/cohorte-route'
 import fabriquantRouter from './routes/fabriquant-route'
 import machineRouter from './routes/machine-route'
 import coachRouter from './routes/coach-route'
+import bcrypt from 'bcrypt'
+import passport from 'passport'
+import passportLocal from 'passport-local'
+const LocalStrategy= passportLocal.Strategy;
 const app: application = express()
 const PORT = process.env.PORT || 3001;
 const cors = require('cors')
@@ -15,6 +19,11 @@ const dotenv = require('dotenv')
 dotenv.config();
 app.use(cors())
 app.use(express.json())
+
+//Config authentication
+// passport.use(new LocalStrategy({usernameField:"email", passwordField: "password"}, async (email, password, done)=>{
+//     try{}catch(){}
+// }))
 app.use("/api/", sessionRouter);
 app.use("/api/", modelRouter);
 app.use("/api/", apprenantRouter);
